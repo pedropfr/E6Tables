1c62746c55d8940392d607960b3edcbc7eee8753
09100440 <09100440@w3332160.portoalegre.pucrsnet.br>
2016-10-20 20:24:14 -0200

    Primeiro Commit

---
+++ E6Tables/src/e6tables/E6Tables.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package e6tables;
+
+/**
+ *
+ * @author 09100440
+ */
+public class E6Tables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
